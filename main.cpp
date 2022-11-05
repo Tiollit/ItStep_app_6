b1fb2453cb2ae84bab6853b2a12c243c23e7d316
Tiollit <Tiollit7@gmail.com>
2022-11-05 16:43:24 -0700

    Lab 31 d 3

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int M, N;
+    int M, N, D, K, H;
     cout << "Incert sizes of arays: M and N" << endl;
     cin >> M >> N;
     int* A = new int[M];
     int* B = new int[N];
     int* C = new int[M + N];
+    D = M + N;
+    K = M + N;
+    H = M + N;
 
     cout << "Generating arrays:" << endl;
     cout << "Array A: " << endl;
@@ -27,6 +30,57 @@ int main()
         cout << *(B + i) << " ";
     }
     cout << endl;
-    
+
+    for (int i = 0; i < M; i++)
+    {
+        *(C + i) = *(A + i);
+    }
+    for (int i = M; i < D; i++)
+    {
+        *(C + i) = *(B + i - M);
+    }
+    cout << "New combined array: " << endl;
+    for (int i = 0; i < D; i++)
+    {
+        cout << *(C + i) << " ";
+    }
+
+    for (int i = 0; i < D; i++)
+    {
+        for (int j = 0; j < D; j++)
+        {
+            if (*(C + i) == *(C + j))
+            {
+                D -= D;
+                //cout << D << " ";
+            }
+        }
+    }
+    cout << endl;
+    cout << "New size of minimal array is: " << D << endl;
+    cout << "Generating array: " << endl;
+    int *S = new int[D];
+    for (int i = 0; i < K; i++)
+    {
+        for (int j = 0; j < K; j++)
+        {
+            if (*(C + i) == *(C + j))
+            { 
+                H -= H;
+                for (int l = j; l < H; l++)
+                {
+                    *(C + l) = *(C + l + 1);
+                }
+                
+            }
+        }
+    }
+    for (int i = 0; i < D; i++)
+    {
+        *(S + i) = *(C + i);
+        cout << *(S + i) << " ";
+    }
+
+
     return 0;
 }
