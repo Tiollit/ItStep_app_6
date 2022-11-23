21fecf2b37ce9358390df957526d22e207e07512
Tiollit <Tiollit7@gmail.com>
2022-11-22 18:28:59 -0800

    g

--- main.cpp
+++ main.cpp
@@ -1,163 +1,52 @@
-//#include <iostream>
-//#include <ctime>
-//using namespace std;
-//template <typename T>
-//T* initArray(int size)
-//{
-//    return new T[size];
-//}
-//template <typename T>
-//void randArray(T* arr, int size)
-//{
-//    for (size_t i = 0; i < size; i++)
-//    {
-//        *(arr + i) = rand() % 10;
-//    }
-//}
-//template <typename T>
-//void printArray(T* arr, int size)
-//{
-//    for (size_t i = 0; i < size; i++)
-//    {
-//        cout << *(arr + i) << "\t";
-//    }
-//    cout << endl;
-//}
-//template <typename T>
-//void deleteArray(T*& arr)
-//{
-//    delete[] arr;
-//    arr = nullptr;
-//}
-//template <typename T>
-//T* initArray(int size)
-//{
-//    return new T[size];
-//}
-//template <typename T>
-//void randArray(T* arr, int size)
-//{
-//    for (size_t i = 0; i < size; i++)
-//    {
-//        *(arr + i) = rand() % 10;
-//    }
-//}
-//template <typename T>
-//void printArray(T* arr, int size)
-//{
-//    for (size_t i = 0; i < size; i++)
-//    {
-//        cout << *(arr + i) << "\t";
-//    }
-//    cout << endl;
-//}
-//template <typename T>
-//void deleteArray(T*& arr)
-//{
-//    delete[] arr;
-//    arr = nullptr;
-//}
-//int main()
-//{
-//    srand(time(0));
-//    int size;
-//    cout << "Enter size->";
-//    cin >> size;
-//    auto arr = initArray<int>(size);
-//    randArray<int>(arr, size);
-//    printArray<int>(arr, size);
-//    deleteArray(arr);
-//    return 0;
-
 #include <iostream>
 #include <ctime>
 using namespace std;
-template <typename T>
-T* initArray(int size)
-{
-    return new T[size];
-}
-template <typename T>
-void randArray(T* arr, int size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        *(arr + i) = rand() % 10;
-    }
-}
-template <typename T>
-void printArray(T* arr, int size)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        cout << *(arr + i) << "\t";
-    }
-    cout << endl;
-}
-template <typename T>
-void deleteArray(T*& arr)
-{
-    delete[] arr;
-    arr = nullptr;
-}
-template <typename T>
-T** initArray(int col, int row)
-{
-    T** new_arr = new T * [col];
-    for (size_t i = 0; i < col; i++)
-    {
-        new_arr[i] = new T[row];
-    }
-    return new_arr;
-}
-template <typename T>
-void randArray(T** arr, int col, int row)
-{
-    for (size_t i = 0; i < col; i++)
-    {
-        for (size_t j = 0; j < row; j++)
-        {
-            arr[i][j] = rand() % 10;
-        }
-    }
-}
-template <typename T>
-void printArray(T** arr, int col, int row)
-{
-    for (size_t i = 0; i < col; i++)
-    {
-        for (size_t j = 0; j < row; j++)
-        {
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-template <typename T>
-void deleteArray(T**& arr, int col)
-{
-    for (size_t i = 0; i < col; i++)
-    {
-        delete[] arr[i];
-    }
-    delete[] arr;
-    arr = nullptr;
-}
+
 int main()
 {
     srand(time(0));
-    int size;
-    cout << "Enter size->";
-    cin >> size;
-    auto arr = initArray<int>(size);
-    randArray<int>(arr, size);
-    printArray<int>(arr, size);
-    deleteArray(arr);
-    cout << "2d arr>------------------" << endl;
-    auto arr2 = initArray<int>(size, size);
-    randArray<int>(arr2, size, size);
-    printArray<int>(arr2, size, size);
-    deleteArray(arr2, size);
+    int M, N;
+    int *K = new int();
+    M = 10, N = 10;
+    int* A = new int[M];
+    int* B = new int[N];   
+
+    cout << "Generating Arrays:" << endl;
+    cout << "Array A: " << endl;
+    for (int i = 0; i < M; i++)
+    {
+        *(A + i) = rand() % 100;
+        cout << *(A + i) << " ";
+    }
+    cout << endl;
+    cout << "Array B: " << endl;
+    for (int i = 0; i < N; i++)
+    {
+        *(B + i) = rand() % 100;
+        cout << *(B + i) << " ";
+    }
+    cout << endl;
+    cout << "Changing arrays: " << endl;
+
+    for (int i = 0; i < M; i++)
+    {
+        *K = *(A + i);
+        *(A + i) = *(B + i);
+        *(B + i) = *K;
+    }
+    cout << "New Arrays: " << endl;
+    cout << "Array A: " << endl;
+    for (int i = 0; i < M; i++)
+    {       
+        cout << *(A + i) << " ";
+    }
+    cout << endl;
+    cout << "Array B: " << endl;
+    for (int i = 0; i < N; i++)
+    {        
+        cout << *(B + i) << " ";
+    }
+    cout << endl;    
+    
     return 0;
 }
